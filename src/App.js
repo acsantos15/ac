28ff4accd32670fe8f246651a1a2abaec8011ddf
ariescarlo15 <ariescarlo113@gmail.com>
2023-12-15 09:32:03 +0800

    analytics fix

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import Landing from './pages/landing.tsx';
 import './fontawesomelib.js';
 import { SpeedInsights } from "@vercel/speed-insights/react"
-import { inject } from '@vercel/analytics';
-
-inject();
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
   return (
     <div className="App">
+      <Analytics />
       <SpeedInsights />
       <Landing />
     </div>
